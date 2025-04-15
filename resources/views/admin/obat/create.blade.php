d0dd65e7e15989d20e8ffb94916b5fdaebc743f2
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2025-04-15 22:13:32 +0700

    add form validation to obat

--- resources/views/admin/obat/create.blade.php
+++ resources/views/admin/obat/create.blade.php
@@ -5,15 +5,30 @@
     @csrf
     <div class="form-group">
       <label for="exampleInputEmail1">Nama Obat</label>
-      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nama" name="name">
+      <input type="text" class="form-control @error('name') is-invalid @enderror"  placeholder="Nama" name="name">
+        @error('name')
+            <div class="invalid-feedback">
+                <span class="text-red-500">{{ $message }}</span>
+            </div>
+        @enderror
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Harga</label>
-      <input type="number" class="form-control" id="exampleInputPassword1" placeholder="0" name="harga">
+      <input type="number" class="form-control @error('harga') is-invalid @enderror" placeholder="0" name="harga">
+      @error('harga')
+        <div class="invalid-feedback">
+            <span class="text-red-500">{{ $message }}</span>
+        </div>
+        @enderror
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Stok</label>
-      <input type="number" class="form-control" id="exampleInputPassword1" placeholder="0" name="stok">
+      <input type="number" class="form-control @error('stok') is-invalid @enderror" placeholder="0" name="stok">
+      @error('stok')
+        <div class="invalid-feedback">
+            <span class="text-red-500">{{ $message }}</span>
+        </div>
+        @enderror
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
--- resources/views/admin/obat/index.blade.php
+++ resources/views/admin/obat/index.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 
+@if (session()->has('success'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ session('success') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+@endif
+
 <div class="card shadow mb-4">
     <div class="card-header py-3">
         <h6 class="m-0 font-weight-bold text-primary mb-3">Data Obat</h6>
